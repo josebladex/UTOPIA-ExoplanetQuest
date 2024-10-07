ba169226a65d179d0b46b3d502956beec2bdf245
Jose Plata <josebladex@gmail.com>
2024-10-06 21:24:56 -0500

    solving errors

--- FrontEnd/app/archive/_components/planets/page.tsx
+++ FrontEnd/app/archive/_components/planets/page.tsx
@@ -1,13 +1,21 @@
-import { columns, Planets } from "./ columns"
-import { DataTable } from "./data-table"
-import data from "@/public/data/data.json"; // Ajusta la ruta según la ubicación real de tu JSON
 
+import { columns, Planets } from "./ columns";
+import { DataTable } from "./data-table";
 
 export default async function Archive() {
+  // Cargar datos desde el archivo JSON
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/data/data.json`);
+
+  // Verificar si la respuesta es correcta
+  if (!res.ok) {
+    throw new Error(`Error en la carga de datos: ${res.status}`);
+  }
+
+  const data = await res.json();
 
   return (
     <div className="container mx-auto py-10">
       <DataTable columns={columns} data={data as Planets[]} />
     </div>
-  )
+  );
 }
